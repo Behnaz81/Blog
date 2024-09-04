1318f7e47ed8e51479502f78e93a309d05c6fc7b
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-04 13:35:08 +0330

    created_at and updated_at fields are added

--- posts/models.py
+++ posts/models.py
@@ -12,3 +12,5 @@ class Post(models.Model):
     title = models.CharField(max_length=255, unique=True)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
