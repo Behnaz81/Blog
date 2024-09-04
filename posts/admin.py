e51d34e3de2f6c73e9a23a09f42db4fa7e237ad2
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-04 15:17:49 +0330

    models are registered in admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from posts.models import Post, Category
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'writer', 'category', 'created_at') 
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.writer = request.user
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Category)  
+
+        
