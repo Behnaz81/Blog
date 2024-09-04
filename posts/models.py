c27a0d5a5489d26f229c3c5de23f117b086a6614
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-04 13:32:23 +0330

    Post and Category models were defined

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
