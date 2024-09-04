6cecd7e2dce86fc689240e3448685a311788d250
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-04 21:20:40 +0330

    index_view was refined

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from django.shortcuts import render
+import requests
 from posts.serializers import PostSerializer, CategorySerializer
 from posts.models import Post, Category
 
@@ -15,3 +17,15 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+def index_view(request):
+    posts_get = requests.get('http://localhost:8000/api/posts/')
+    posts = posts_get.json()
+    categories_get = requests.get('http://localhost:8000/api/categories/')
+    categories = categories_get.json()
+
+    context = {
+        'posts': posts,
+        'categories': categories
+    }
+
+    return render(request, 'index.html', context)
