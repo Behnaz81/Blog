a24cae0f2437f705d2412b6862670bd4b40028a7
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-04 16:17:47 +0330

    urls were defined for categories

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from posts.views import PostViewSet
+from posts.views import PostViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
+router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
