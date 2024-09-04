2f4bdfad92b6af8c99e58873a7eec79d68bd1ed0
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-04 15:02:03 +0330

    models were updated

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,14 @@ User = get_user_model()
 class Category(models.Model):
     title = models.CharField(max_length=100)
 
+    
+    class Meta:
+        verbose_name_plural = "Categories"
 
+    def __str__(self):
+        return self.title
+    
+    
 class Post(models.Model):
     title = models.CharField(max_length=255, unique=True)
     body = models.TextField()
@@ -15,3 +22,6 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
