f570d3cf2b49cf24fb5e9f61b72735fc7ea951ce
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-04 21:21:56 +0330

    urls were defined

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from posts.views import PostViewSet, CategoryViewSet
+from posts.views import PostViewSet, CategoryViewSet, index_view
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -8,5 +8,6 @@ router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('index/', index_view, name="index"),
 ]
 
