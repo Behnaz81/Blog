503b67b6d3832bad3ec3bf557459a41c03e55a73
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-04 16:17:02 +0330

    CategorySerializer was defined

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from posts.models import Post
+from posts.models import Post, Category
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,3 +7,10 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ('id', 'title', 'body', 'category', 'writer', 'created_at', 'updated_at')
         read_only_field = ('id', 'writer', 'created_at', 'updated_at')
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'title')
+        read_only_field = ('id')
+        
