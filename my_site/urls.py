c2342f61a53f83e957475400b4747813eb5be4ab
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-05 12:50:36 +0330

    filtering posts based on category

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from my_site.views import index_view
+from my_site.views import index_view, filtered_posts
+
+app_name = "my_site"
 
 urlpatterns = [
     path('', index_view, name="index"),
+    path('<int:cat_id>/', filtered_posts, name="filtered"),
 ]
--- my_site/views.py
+++ my_site/views.py
@@ -15,3 +15,16 @@ def index_view(request):
     }
 
     return render(request, 'index.html', context)
+
+def filtered_posts(request, cat_id):
+    posts_get = requests.get(f'http://localhost:8000/api/posts-with-category/{cat_id}/')
+    posts = posts_get.json()
+    categories_get = requests.get('http://localhost:8000/api/categories/')
+    categories = categories_get.json()
+
+    context = {
+        'posts': posts,
+        'categories': categories
+    }
+
+    return render(request, 'index.html', context)
