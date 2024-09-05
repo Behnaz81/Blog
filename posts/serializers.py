e36c9a02b68113a673bd732521020eae5d176e24
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-05 12:03:27 +0330

    displaying category

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,18 +1,26 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from posts.models import Post, Category
+
+User = get_user_model()
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('id', 'title')
+        read_only_field = ('id')
 
 
 class PostSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%d %b")
+    category = CategorySerializer(read_only=True)
 
     class Meta:
         model = Post
         fields = ('id', 'title', 'body', 'category', 'writer', 'created_at', 'updated_at', 'image')
         read_only_field = ('id', 'writer', 'created_at', 'updated_at')
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('id', 'title')
-        read_only_field = ('id')
+
         
