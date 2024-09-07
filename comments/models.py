90ba35413a28bd946e7c66401309eda3ae583e82
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-07 17:12:30 +0330

    Comment model was corrected

--- comments/models.py
+++ comments/models.py
@@ -9,3 +9,13 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     message = models.TextField()
+    seen = models.BooleanField(default=False)
+    display = models.BooleanField(default=False)
+    posted_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return f'{self.message} by {self.user}'
+    
+
+    class Meta:
+        ordering = ['posted_at']
