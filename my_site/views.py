9a18fd00732e6ea8b2284e73e3c15d3cdf742fa3
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-07 16:35:29 +0330

    related posts are added to the detail page

--- my_site/views.py
+++ my_site/views.py
@@ -33,11 +33,17 @@ def detail_post(request, post_id):
     post_get = requests.get(f'http://localhost:8000/api/posts/{post_id}/')
     post = post_get.json()
     categories_get = requests.get('http://localhost:8000/api/categories/')
+    cat_id = post['category']['id']
     categories = categories_get.json()
+    related_posts = requests.get(f'http://localhost:8000/api/posts-with-category/{cat_id}/')
+    related_posts_json = related_posts.json()
+
+    related_posts_json = [i for i in related_posts_json if not (i['id'] == post['id'])][0:5]
 
     context = {
         'post': post,
-        'categories': categories
+        'categories': categories,
+        'related_posts': related_posts_json
     }
 
     return render(request, 'post_details.html', context)
