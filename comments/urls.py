b9565ed72b863148bac33017ff4a082698b9a90f
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-10 17:06:15 +0330

    an api was provided to create the comment object

--- comments/urls.py
+++ comments/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from comments.views import CommentFilteredByPost
+from comments.views import CommentFilteredByPost, CreateCommentView
 
 urlpatterns = [
     path('comments-filtered-by-post/<int:post_id>/', CommentFilteredByPost.as_view(), name="comment-filtered-by-post"),
+    path('comment-add/<int:post_id>/', CreateCommentView.as_view(), name="add-comment"),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-from comments.serializers import CommentSerializer
+from comments.serializers import CommentSerializer, CommentCreateSerializer
 from comments.models import Comment
+from posts.models import Post
 
 
 class CommentFilteredByPost(generics.ListAPIView):
@@ -10,3 +11,12 @@ class CommentFilteredByPost(generics.ListAPIView):
     def get_queryset(self):
         post_id = self.kwargs.get('post_id') 
         return Comment.objects.filter(post=post_id).filter(display=True)
+
+
+class CreateCommentView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentCreateSerializer
+
+    def perform_create(self, serializer):
+        post = Post.objects.get(id=self.kwargs.get('post_id'))
+        return serializer.save(post=post)
