6316c6240e8042306a983c56ad4e32e0529a4a5b
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-10 16:58:48 +0330

    updated the Comment model

--- comments/models.py
+++ comments/models.py
@@ -7,15 +7,15 @@ User = get_user_model()
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, editable=False)
     message = models.TextField()
     seen = models.BooleanField(default=False)
     display = models.BooleanField(default=False)
     posted_at = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
-        return f'{self.message} by {self.user}'
+        return f'{self.message} by {self.user} for {self.post}'
     
 
     class Meta:
-        ordering = ['posted_at']
+        ordering = ['-posted_at']
