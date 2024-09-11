1f5d0f141475a951af92d1b5bcf8a21ac44fb5be
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-11 18:22:17 +0330

    RegisterView was defined

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from users.serializers import CustomUserSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+
+            return Response({'user': serializer.data, 'token': token.key}, 
+                            status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, 
+                        status=status.HTTP_400_BAD_REQUEST)
+
+
