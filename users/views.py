c34ca7c60ab812e3891eb4ed845aa92ef03dcfad
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-09-12 14:53:06 +0330

    logout api was provided

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 from users.serializers import RegisterSerializer, LoginSerializer 
 
@@ -42,3 +43,19 @@ class LoginView(APIView):
         
         return Response(serializer.errors, 
                         status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            request.user.auth_token.delete()
+
+            return Response({'details': 'Successfully logged out'}, 
+                            status=status.HTTP_200_OK)
+        
+        except (AttributeError, Token.DoesNotExist):
+            return Response({'details': 'No token found'}, 
+                            status=status.HTTP_400_BAD_REQUEST)
